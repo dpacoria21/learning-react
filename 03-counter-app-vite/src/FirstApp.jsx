0d071f05be1de4d7bcc2eb0bf878df57cfe9754a
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-17 00:07:05 -0500

    Se utilizo variables y se mostraron en el html con el {}

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,6 +1,18 @@
 import { Fragment } from "react";
 
+const newMessage = {
+    name: 'Diego',
+    edad: 20
+};
+
+const getSaludo = (a, b) => { //Mientras estas funciones no dependan de nada van afuera
+    return a+b;
+}
+
 export const FirstApp = () => {
+
+    const name = 'Diego Pacori'; // si el name es usado por una funcion, estas se deben crear aqui
+
     return (
         //Cabe resaltar que siempre se espera un nodo padre
 
@@ -12,7 +24,7 @@ export const FirstApp = () => {
 
         //Forma moderna (sin importa el Fragment) usando un agrupador
         <>
-            <h1>Diego Pacori: Primer Componente en React</h1>
+            <h1>{ getSaludo(10, newMessage.edad) }</h1>
             <p>Hola soy un Cacahuate</p>
         </>
     );
