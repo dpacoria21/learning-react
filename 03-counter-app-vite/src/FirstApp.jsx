42cf2a531301bb9c27d540589868988f0029e135
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-16 23:41:21 -0500

    2 formas de mostrar mas de una etiqueta en React

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,4 +1,19 @@
+import { Fragment } from "react";
 
 export const FirstApp = () => {
-    return (<h1>Diego Pacori: Primer Componente en React</h1>);
+    return (
+        //Cabe resaltar que siempre se espera un nodo padre
+
+        // Usando Fragmentos (Forma antigua)
+        // <Fragment>
+        //     <h1>Diego Pacori: Primer Componente en React</h1>
+        //     <p>Hola soy un Cacahuate</p>
+        // </Fragment>        
+
+        //Forma moderna (sin importa el Fragment) usando un agrupador
+        <>
+            <h1>Diego Pacori: Primer Componente en React</h1>
+            <p>Hola soy un Cacahuate</p>
+        </>
+    );
 }
