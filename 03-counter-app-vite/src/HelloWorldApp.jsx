bd1c8976f5af4a1963df8b8ffea07f20da9189c7
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-16 23:27:44 -0500

    Primer componente en React

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,15 @@
+// function App () {
+//     return ( <h1>Hola Mundo!</h1> );
+// }
+
+// export {
+//     App as default
+// }
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App!!!</h1>
+  )
+}
