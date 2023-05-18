3625e60ebcdf377a59db2e0b4fd259c05b155fca
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-18 02:05:48 -0500

    Eventos en React

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -2,10 +2,20 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ( {value} ) => {
+
+    const handlerAdd = (e) => {
+        value++;
+        console.log(value);
+    }
+
     return(
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+
+            <button onClick={ handlerAdd }>
+                +1
+            </button>
         </>
     )
     
