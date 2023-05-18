ba576444ddf2545016a99fd8a3fbd4ec2e8016d9
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-18 02:18:25 -0500

    Uso del hook useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,17 +1,19 @@
-
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ( {value} ) => {
 
-    const handlerAdd = (e) => {
-        value++;
-        console.log(value);
+    const [ counter, setCounter ] = useState( value );
+
+    const handlerAdd = () => {
+        setCounter( counter + 1 );
+        // setCounter( (c) => c+1 )
     }
 
     return(
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             <button onClick={ handlerAdd }>
                 +1
