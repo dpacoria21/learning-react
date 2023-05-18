0d10402443f4f3ffec2f96cbd300be05ccb526d3
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-18 01:51:35 -0500

    Se realizo la tarea de propTypes correctamente

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {HelloWorldApp} from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 
 import './styles.css';
 
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={5} />
     </React.StrictMode>
 )
