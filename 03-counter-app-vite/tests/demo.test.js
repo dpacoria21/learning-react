50d1cbca38d64654c34da3799359d291ebcd3cc4
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-18 22:39:33 -0500

    Ejemplo de una prueba sin ningun sentido

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,7 +1,17 @@
+describe('Pruebas en <DemoComponent />', () => {
+    
+    test('Esta prueba no debe de fallar', () => {
+        
+        // 1. Inicializacion
+        const message1 = 'Hola Mundo';
+    
+        // 2. Estimulo
+        const message2 = message1.trim();
+    
+        // 3. Observar el comportamiento... esperado
+        // expect( message1 ).toBe(message2);
+        expect(message1).toBe(message2);
+    
+    });
 
-
-test('Esta prueba no debe de fallar', () => {
-    if(1 === 0) {
-        throw new Error('Division entre 0 no es posible');
-    }
 })
