a95641157440bf111b289bf13a824f3ce8d3ef93
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-21 01:43:54 -0500

    Se hizo test al compoennte FirstApp y se comopraro primeropsu contenido  con el .toMatchSanpshot() y otros usando el getByText y el innerHTML

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -3,13 +3,22 @@ import { FirstApp } from "../src/FirstApp";
 
 describe('Pruebas en FirstApp', () => {
     test('Debe de hacer match con el snapshor', () => {
+        // Evaluar la forma de componente y que este no cambie
+        // Toma una fotografia y luego la compara
+        const title = 'Hola, soy German';
+        const { container } = render(<FirstApp title={ title } />);  
+        // console.log(container);
+        expect(container).toMatchSnapshot();      
 
-        const title = 'Hola, soy German Garmendia';
+    });
+    test('Debe de mostrar el titulo en un h1', () => {
+        const title = 'Hola, soy German';
         const { container, getByText } = render( <FirstApp title={ title } /> );
+        // getByText, busca un nodo por su contenido
+        // console.log(getByText(title));
         expect( getByText(title) ).toBeTruthy();
 
         const h1 = container.querySelector('h1');
         expect(h1.innerHTML).toContain( title );
-
-    });
+    })
 });
