914e8b1c78084ceb5372a3607a744e699dfb9f4c
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-21 19:10:11 -0500

    Se realizo el renderizado de una lista que forma parte del estado y creamos una funcion para poder agregar solo 1 categoria

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+ 
+    const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        // setCategories( categories );
+        setCategories( categories.concat('CSGO') );
+        // setCategories( [...categories, 'CSGO'] );
+        // setCategories( cat => [...cat, 'CSGO'] )
+    }
+
+    console.log(categories);
+
     return (
         <>
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listadodo de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                { categories.map((category) => {
+                    return <li key={category} >{category}</li>;
+                }) }
+                <li>ABC</li>
+                <li>123</li>
+                <li>xyz</li>
+            </ol>
         </>
     )
 }
