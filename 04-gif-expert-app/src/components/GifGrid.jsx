0d063601f48c732418f9202f605885773dfe4398
Diego Pacori <dpacoria@unsa.edu.pe>
2023-05-24 00:19:43 -0500

    Se creo un loader al momento de realizar busquedas y un archivo de barril para las exportaciones

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,30 +1,18 @@
-import { getGifs } from "../helpers/getGifs";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
-
-import { useEffect, useState } from "react";
 
 
 export const GifGrid = ({category}) => {   
 
     const {images, isLoading} = useFetchGifs(category); 
-    // console.log(images, isLoading);
-    // const [images, setImages] = useState([]);
-
-    // const getImages = async() => {
-    //     const newImages = await getGifs(category);
-    //     setImages(newImages);
-    // }
-
-    // useEffect(() => {
-    //     getImages();
-    // }, []);
-
-    // console.log(counter);
 
     return (
         <>
-            <h3>{category}</h3>        
+            {
+                isLoading && <h3>Cargando...</h3>
+            }
+            <h3>{category}</h3>     
+
             <div className="card-grid" >
                 {images.map((image) => (
                     <GifItem key={image.id} {...image}/>
