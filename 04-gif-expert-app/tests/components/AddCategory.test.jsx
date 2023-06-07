17691d67b8661a9abf939885bed12ce692bb6557
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-06 21:12:24 -0500

    Se simulo un submit con los fireEvent y se vio si la funcion es llamada o no

--- 04-gif-expert-app/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import { AddCategory } from "../../src/components/AddCategory";
 
 describe('Testing in AddCategory.jsx', () => { 
@@ -9,6 +9,44 @@ describe('Testing in AddCategory.jsx', () => {
         const input = screen.getByRole('textbox');
         fireEvent.input(input, {target: {value: 'Saitama'}});
         expect(input.value).toBe('Saitama');
+    });
+
+    test('Debe de llamar onNewCategory si el input tiene un valor', () => {
+        const inputValue = 'Saitama';
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={(onNewCategory)} />);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, {target: {value: inputValue}});
+        fireEvent.submit(form);
+        screen.debug();
+        expect(input.value).toBe('');
+        
+        expect(onNewCategory).toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);
 
     });
+
+    test('No debe de llamar el onNewCategory si el input esta vacio', () => {
+
+        const inputValueEmpty = '';
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={onNewCategory} />)
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, {target: {value: inputValueEmpty}});
+        fireEvent.submit(form);
+
+        expect(input.value).toBe(inputValueEmpty);
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
+
+
+    })
 });
