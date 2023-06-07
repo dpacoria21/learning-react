477ab85ea7bc7411c1ad82f914598ee918560d1b
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-06 19:16:02 -0500

    Haciendo testing a una llamada a una API

---
+++ 04-gif-expert-app/tests/helpers/getGifs.test.jsx
@@ -0,0 +1,13 @@
+import { getGifs } from "../../src/helpers/getGifs"
+
+describe('Probando el getGifs', () => {
+    test('trayendo data del getGifs', async () => {
+        const gifs = await getGifs('One punch');
+        expect(gifs.length).toBeGreaterThan(0);
+        expect(gifs[0]).toEqual({
+            id: expect.any(String),
+            title: expect.any(String),
+            url: expect.any(String)
+        })
+    })
+})
