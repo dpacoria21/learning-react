c5f62ae7bdb0e1e682961b9c317ca6444f142148
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-12 17:39:15 -0500

    Se creo el proyecto HooksApp y se realizo la tarea del contador, al usar useState lo que tiene que cambiar es el objeto, porque asi lo guardamos en el estado

---
+++ 05-hook-app/src/HooksApp.jsx
@@ -0,0 +1,7 @@
+export const HooksApp = () => {
+    return (
+        <>
+            <h1>Hola Mundo desde HooksApp</h1>
+        </>
+    )
+}
---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import  ReactDOM from "react-dom/client";
+
+import './index.css';
+import { HooksApp } from "./HooksApp";
+import { CounterApp } from "./01-useState/CounterApp";
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <React.StrictMode>
+    <>
+      <CounterApp />
+    </>
+  </React.StrictMode>
+)
