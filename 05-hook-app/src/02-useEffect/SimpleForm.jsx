af100fa33b23dfce21264befe979a5f7607c6267
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-12 18:29:01 -0500

    Se utilizo el useEffect para realizar alguna accion cuando cambie un valor que nosotros le especificamos

--- 05-hook-app/src/02-useEffect/SimpleForm.jsx
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -1,15 +1,55 @@
-export const SimpleForm = () => {
-  return (
-    <>
-        <h1>Formulario Simple</h1>
-        <hr />
+import { useEffect, useState } from "react"
 
-        <input 
-            type="text" 
-            className="form-control"
-            placeholder="Username"
-            name="username"
-        />
-    </>
-  )
+export const SimpleForm = () => {
+    
+    const [formState, setFormState] = useState({
+        username: 'strider',
+        email: 'noleasesto@outlook.com',
+    });
+
+    const {username, email} = formState;
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target; 
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        console.log('UseEffect called!');
+    }, []);
+
+    useEffect(() => {
+        console.log('FormState changed!');
+    }, [formState]);
+
+    useEffect(() => {
+        console.log('Email Changed!');
+    }, [email]);
+
+    return (
+        <>
+            <h1>Formulario Simple</h1>
+            <hr />
+
+            <input 
+                type="text" 
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input 
+                type="email" 
+                className="form-control mt-2"
+                placeholder="noleas@outlook.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
 }
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import  ReactDOM from "react-dom/client";
 
 import './index.css';
-import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <>
-      <CounterWithCustomHook />
+      <SimpleForm />
     </>
   </React.StrictMode>
 )
