a6b85b4cadf0f5b70a078243b5ccff0090af0a8e
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-17 10:59:13 -0500

    Se trabajo con el useMemo de react para hacer que un trabajo muy pesado no se vuelva a cargar si este no ha sufrido cambios

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -7,15 +7,16 @@ import './index.css';
 // import { FormWithCustomHook } from "./02-useEffect/formWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
-import { Layout } from "./05-useLayoutEffect/Layout";
-import { Memorize } from "./06-memos/Memorize";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { Memorize } from "./06-memos/Memorize";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
+import { MemoHook } from "./06-memos/MemoHook";
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   // <React.StrictMode>
     <>
-      <Memorize />
+      <MemoHook />
     </>
   // </React.StrictMode>
 )
