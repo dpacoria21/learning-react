ee3f3e1ed8f172960e7c81f44c586194338700e9
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-17 10:46:50 -0500

    Se utilizo el memo para verificar que un componente no haya sufrido cambios y no se vuelva a cargar

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -8,13 +8,14 @@ import './index.css';
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 import { Layout } from "./05-useLayoutEffect/Layout";
+import { Memorize } from "./06-memos/Memorize";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   // <React.StrictMode>
     <>
-      <Layout />
+      <Memorize />
     </>
   // </React.StrictMode>
 )
