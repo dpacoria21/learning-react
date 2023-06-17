2fc7a642f930e6f1f8c6abf8cbc9d4f4a888942d
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-16 23:14:52 -0500

    Se trabajo con el useRef para poder seleccionar un elemento del html con una clase especial

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,14 +5,15 @@ import './index.css';
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/formWithCustomHook";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   // <React.StrictMode>
     <>
-      <MultipleCustomHooks />
+      <FocusScreen />
     </>
   // </React.StrictMode>
 )
