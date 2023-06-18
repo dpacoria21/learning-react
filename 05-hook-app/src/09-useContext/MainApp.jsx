94e07071957d531da759fb39c1bb12f3a48103cb
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-18 10:07:38 -0500

    Se creo el contexto el cual es un usuario y se actualizo desde el login y el home tambien visualiza los cambios, sirve para repartir informacion sin necesidad de utilizar puentes

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -3,15 +3,17 @@ import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
 import { AboutPage } from './AboutPage';
 import { Navbar } from "./Navbar";
+import { UserProvider } from "./context/UserProvider";
+
+
 export const MainApp = () => {
     return(
-        <>
+        <UserProvider>
             {/* <Link to="/">Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/login'>Login</Link> */}
             <Navbar />
             <hr />
-
 
             <Routes>
                 <Route path="/" element={ <HomePage /> }/>
@@ -22,6 +24,6 @@ export const MainApp = () => {
                 <Route path="/*" element={ <Navigate to={"/about"} /> } />
             </Routes>
 
-        </>
+        </UserProvider>
     )
 }
