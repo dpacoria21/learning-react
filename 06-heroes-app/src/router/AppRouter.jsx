3e85163d759ceeb9ae7737367056c4b282a5639c
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-20 13:16:57 -0500

    Se creo un AppRouter para poder tener todo distribuido y que sea facil de buscar entre archivos

--- 06-heroes-app/src/router/AppRouter.jsx
+++ 06-heroes-app/src/router/AppRouter.jsx
@@ -1,17 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+import {MarvelPage, DcPage} from '../heroes';
+import { LoginPage } from "../auth";
+import { Navbar } from "../ui";
+// import { MarvelPage } from "../heroes/pages/MarvelPage"
+// import { DcPage } from "../heroes/pages/DcPage"
+// import { LoginPage } from "../auth/pages/LoginPage"
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />
+
             <Routes>
                 <Route path="/marvel" element={<MarvelPage />}/>
                 <Route path="/dc" element={<DcPage />}/>
                 <Route path="/login" element={<LoginPage />}/>
-                <Route path="*" element={<Navigate to='/marvel' /> } />
+                <Route path="*" element={<Navigate to='/marvel'/>} />
             </Routes>
         </>
     )
