4e5304e573a41086adac66322be11971006e6ac8
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-20 14:41:43 -0500

    Se trabajo con la navegacion usando el useNavigate

--- 06-heroes-app/src/auth/pages/LoginPage.jsx
+++ 06-heroes-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom"
+
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const onLogin = () => {
+        navigate('/marvel', {
+            replace: true,
+        })
+    }
+    
     return (
-        <>
-            <h1>Saludos desde el Login</h1>
-        </>
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr />
+
+            <button onClick={onLogin} className="btn btn-primary">
+                Login
+            </button>
+        </div>
     )
 }
