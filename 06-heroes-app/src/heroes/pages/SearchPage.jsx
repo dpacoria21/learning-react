0eefaed6e140630fb539f706218bdd3b6331ec89
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-21 00:05:18 -0500

    Se utilizo una libreria para controlar los parametros de la url y se uso el useLocation para pasarle esos datos y el useNavigate para ingresar esos datos en la url

--- 06-heroes-app/src/heroes/pages/SearchPage.jsx
+++ 06-heroes-app/src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,28 @@
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components"
 
 export const SearchPage = () => {
+
+    const { searchText, onInputChange } = useForm({
+        searchText: '',
+    });
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { q='' } = queryString.parse(location.search);
+
+    // console.log(location);
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        if(searchText.trim().length <= 1 ) return;
+        navigate(`?q=${searchText.toLowerCase().trim()}`);
+    }
+
     return (
         <>
         
@@ -12,13 +34,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form action="">
+                    <form onSubmit={onSearchSubmit}>
                         <input 
                             type="text" 
                             placeholder="Search a hero"
                             className="form-control"
                             name="searchText"
                             autoComplete="off"
+                            value={searchText}
+                            onChange={onInputChange}
                         />
 
                         <button className="btn btn-outline-primary mt-1">
@@ -36,7 +60,7 @@ export const SearchPage = () => {
                     </div>
 
                     <div className="alert alert-danger">
-                        No hero with <b>ABC</b>
+                        No hero with <b>{q}</b>
                     </div>
 
                     {/* <HeroCard /> */}
