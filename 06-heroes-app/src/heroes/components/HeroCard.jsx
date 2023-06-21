5b7ff59018aabb09318e4105951d9d0123d3c9a5
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-20 23:29:22 -0500

    Se uso animaciones de css de una libreria para darle mas interactividad a la pagina

--- 06-heroes-app/src/heroes/components/HeroCard.jsx
+++ 06-heroes-app/src/heroes/components/HeroCard.jsx
@@ -18,11 +18,11 @@ export const HeroCard = ({
     // const charactesByHero = (<p>{characters}</p>);
 
     return(
-        <div className="col">
+        <div className="col animate__animated animate__fadeIn">
             <div className="card">
                 <div className="row no-gutters">
 
-                    <div className="col-4">
+                    <div className="col-4 animate__animated animate__bounce">
                         <img src={heroImageUrl} className="card-img" alt={superhero} />
                     </div>
 
