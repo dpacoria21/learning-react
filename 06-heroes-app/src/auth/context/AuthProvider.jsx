58be863de83a269b73dbf1e5047a557407620c2f
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-23 09:39:30 -0500

    Se creo el context y el reducer para manejar el estado del login y dependiendo si esta o no logeado se muestra su nombre en la pantalla

---
+++ 06-heroes-app/src/auth/context/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { AuthReducer } from "./AuthReducer"
+import { types } from "../types/types"
+
+const initialState = {
+    logged: false,
+}
+
+export const AuthProvider = ({children}) => {
+    
+    const [authState, dispatch] = useReducer(AuthReducer, initialState);
+
+    const login = (name = '') => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 'ABC',
+                name
+            }
+        }
+        dispatch(action);
+    }
+
+    return (
+        <AuthContext.Provider value={{ 
+            authState,
+            login,
+         }}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
--- 06-heroes-app/src/ui/components/Navbar.jsx
+++ 06-heroes-app/src/ui/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../auth/context";
 
 export const Navbar = () => {
 
     const navigate = useNavigate();
 
-
+    const {authState : {logged, user: {id = '', name} = {}}} = useContext(AuthContext);
 
     const onLogout = () => {
         navigate('/login', {
@@ -60,7 +62,7 @@ export const Navbar = () => {
                 <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                     <ul className="navbar-nav ml-auto">
                         <span className="nav-item nav-link text-info">
-                            Diego
+                            {logged ? name : 'usuario'}
                         </span>
                         <button onClick={onLogout} className="nav-item nav-link btn">
                             Logout
