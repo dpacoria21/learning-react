35bd5e28f3f4a43d3aeaa68aa7e0a59a4ce390b7
Diego Pacori <dpacoria@unsa.edu.pe>
2023-06-23 10:57:11 -0500

    Se implemento las rutas privadas y publicas dependiendo si el usuario esta loggeado o no

--- 06-heroes-app/src/router/AppRouter.jsx
+++ 06-heroes-app/src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 
 import { HeroesRoutes} from '../heroes';
 import { LoginPage } from "../auth";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 // import { MarvelPage } from "../heroes/pages/MarvelPage"
 // import { DcPage } from "../heroes/pages/DcPage"
 // import { LoginPage } from "../auth/pages/LoginPage"
@@ -11,8 +13,34 @@ export const AppRouter = () => {
         <>
             
             <Routes>
-                <Route path="/login" element={<LoginPage />}/>
-                <Route path="/*" element={<HeroesRoutes />}/>
+
+
+
+                {/* Segunda forma de hacerlo
+                    <Route path="/login/*" element={
+                    <PublicRoute>
+                        <Routes>
+                            <Route path="/*" element={<LoginPage />}/>
+                        </Routes>
+                    </PublicRoute>
+                }/> */}
+
+                {/* Forma mas corta */}
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                }/>
+
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                }/>
+
+                {/* <Route path="/login" element={<LoginPage />}/> */}
+                {/* <Route path="/*" element={<HeroesRoutes />}/> */}
+
             </Routes>
         </>
     )
